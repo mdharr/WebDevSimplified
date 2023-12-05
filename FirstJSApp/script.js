63b8940c66ad2557057d2ff71e4e859e9cb65dbc
Michael Harrington <mdharr7@gmail.com>
2023-12-05 21:17:24 +0800

    Complete less 45 on creating a modal with overlay and event listeners to open and close on click.

--- FirstJSApp/script.js
+++ FirstJSApp/script.js
@@ -1,24 +1,30 @@
-// 1. Select all elements
-const form = document.querySelector('#new-item-form');
-const list = document.querySelector('#list');
-const input = document.querySelector('#item-input');
-const listItems = list.children;
+//   TODO: 2. Select the elements with the following IDs
+//     * modal
+//     * open-modal-btn
+//     * close-modal-btn
+//     * BONUS: overlay
+const modal = document.querySelector('#modal');
+const openModalBtn = document.querySelector('#open-modal-btn');
+const closeModalBtn = document.querySelector('#close-modal-btn');
+const overlay = document.querySelector('#overlay');
 
-// 2. When I submit a form add a new element
-form.addEventListener('submit', e => {
-    e.preventDefault();
+// TODO: 3. Create a click event listener for the open-modal-btn that adds the class "open" to the modal
+// BONUS: Also add the class "open" to the overlay
+openModalBtn.addEventListener('click', openModal);
 
-    // 1. Create a new item
-    const item = document.createElement('div');
-    item.innerText = input.value;
-    item.classList.add('list-item');
+// TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
+// BONUS: Also remove the class "open" from the overlay
+closeModalBtn.addEventListener('click', closeModal);
 
-    // 2. Add that item to the list
-    list.appendChild(item);
-    // 3. Clear input
-    input.value = '';
-    // 4. Setup event listener to delete item when clicked
-    item.addEventListener('click', () => {
-        list.removeChild(item);
-    })
-});
+// BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
+overlay.addEventListener('click', closeModal);
+
+function closeModal() {
+    modal.classList.remove('open');
+    overlay.classList.remove('open');
+}
+
+function openModal() {
+    modal.classList.add('open');
+    overlay.classList.add('open');
+}
