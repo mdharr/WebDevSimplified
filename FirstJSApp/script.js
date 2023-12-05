c4e3841108ec61bd7ac040a8514b00b3fab47b04
Michael Harrington <mdharr7@gmail.com>
2023-12-05 20:50:59 +0800

    Complete lesson 43 on Simple Todo List.

--- FirstJSApp/script.js
+++ FirstJSApp/script.js
@@ -1,4 +1,24 @@
-const grandParent = document.querySelector('#grand-parent');
-const children = grandParent.querySelectorAll('.child');
+// 1. Select all elements
+const form = document.querySelector('#new-item-form');
+const list = document.querySelector('#list');
+const input = document.querySelector('#item-input');
+const listItems = list.children;
 
-children.forEach(child => child.style.color = 'red');
+// 2. When I submit a form add a new element
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // 1. Create a new item
+    const item = document.createElement('div');
+    item.innerText = input.value;
+    item.classList.add('list-item');
+
+    // 2. Add that item to the list
+    list.appendChild(item);
+    // 3. Clear input
+    input.value = '';
+    // 4. Setup event listener to delete item when clicked
+    item.addEventListener('click', () => {
+        list.removeChild(item);
+    })
+});
